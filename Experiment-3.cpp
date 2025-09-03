9df41f30a078366bbaa45f7384c52e1009236e93
Tanujjangir06 <tanujjangir06@gmail.com>
2025-09-03 22:29:44 +0530

    Update Experiment-3.cpp

--- Experiment-3.cpp
+++ Experiment-3.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main()
 {
-    int m = 3, n = 3;                                    // max powers of x and y
-    int poly1[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; // example polynomial 1
-    int poly2[3][3] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}}; // example polynomial 2
+    int m = 3, n = 3;                                    
+// max powers of x and y
+    int poly1[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; 
+// example polynomial 1
+    int poly2[3][3] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}}; 
+// example polynomial 2
     int sum[3][3];
 
     // Polynomial addition
